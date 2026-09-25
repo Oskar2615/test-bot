67654a175cf270796f6c885eca68f681704111ac
Oskar2615 <Oskar_beh@proton.me>
2026-09-25 19:35:46 +0200

    Bot Code geändert

--- bot.py
+++ bot.py
@@ -1,29 +1,25 @@
+import os
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-
 bot = commands.Bot(
-    commands_prefix=".",
-
+    command_prefix=".",
     intents=discord.Intents.default()
 )
 
-
 @bot.event
 async def on_ready():
-    print(f"online als {bot.user}")
+    print(f"Online als {bot.user}")
     await bot.tree.sync()
 
-
-@bot.tree.command(name="ping",
-description="zeigt den bot ping")
+@bot.tree.command(
+    name="ping",
+    description="Zeigt den Bot-Ping"
+)
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("🏓 Pong!")
 
-
-
-   bot.run(TOKEN)
+bot.run(TOKEN)
